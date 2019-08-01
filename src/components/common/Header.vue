0038acf322a449f9fcfd815d825baa665f680812
dylan880507 <758777282@qq.com>
2019-08-01 18:59:35 +0800

    test
    Signed-off-by: dylan880507 <758777282@qq.com>

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -49,7 +49,6 @@ export default {
             this.$router.push('/notification');
         },
         goForumMsgPage:function () {
-            console.log("论坛消息......");
             this.$router.push('/forumNotification');
         },
     }
