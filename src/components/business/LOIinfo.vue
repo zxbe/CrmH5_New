0fe0ac67e1c561a45e41e64a770f23167be49827
dylan880507 <758777282@qq.com>
2019-08-14 16:31:27 +0800

    对接loi逻辑
    Signed-off-by: dylan880507 <758777282@qq.com>

--- src/components/business/LOIinfo.vue
+++ src/components/business/LOIinfo.vue
@@ -11,21 +11,21 @@
             <div class="label">
                 <span class="lanText" data-lanid="563_编号"></span><span>:</span>
             </div>
-            <div class="item-value">{{dataModel.bianh}}</div>
+            <div class="item-value" v-html="dataModel.bianh"></div>
         </div>
 
         <div class="item f14">
             <div class="label">
                 <span class="lanText" data-lanid="710_标题"></span><span>:</span>
             </div>
-            <div class="item-value">{{dataModel.requestname}}</div>
+            <div class="item-value" v-html="dataModel.requestname"></div>
         </div>
 
         <div class="item f14">
             <div class="label">
                 <span class="lanText" data-lanid="1000257_提交人"></span><span>:</span>
             </div>
-            <div class="item-value">{{dataModel.loginid}}</div>
+            <div class="item-value" v-html="dataModel.loginid"></div>
         </div>
 
         <div class="item f14">
@@ -39,56 +39,56 @@
             <div class="label">
                 <span class="lanText" data-lanid="1000259_职务"></span><span>:</span>
             </div>
-            <div class="item-value">{{dataModel.jobtitlename}}</div>
+            <div class="item-value" v-html="dataModel.jobtitlename"></div>
         </div>
 
         <div class="item f14">
             <div class="label">
                 <span class="lanText" data-lanid="1000260_所属部门"></span><span>:</span>
             </div>
-            <div class="item-value">{{dataModel.departmentname}}</div>
+            <div class="item-value" v-html="dataModel.departmentname"></div>
         </div>
 
         <div class="item f14">
             <div class="label">
                 <span class="lanText" data-lanid="1000277_项目名称"></span><span>:</span>
             </div>
-            <div class="item-value">{{dataModel.xiangmmc}}</div>
+            <div class="item-value" v-html="dataModel.xiangmmc"></div>
         </div>
 
         <div class="item f14">
             <div class="label">
                 <span class="lanText" data-lanid="1000261_计划签署日期"></span><span>:</span>
             </div>
-            <div class="item-value">{{dataModel.jihqsrq}}</div>
+            <div class="item-value" v-html="dataModel.jihqsrq"></div>
         </div>
 
         <div class="item f14">
             <div class="label">
                 <span class="lanText" data-lanid="1000262_项目亮点及航空公司背景简介"></span><span>:</span>
             </div>
-            <div class="item-value">{{dataModel.xiangmldjbj}}</div>
+            <div class="item-value" v-html="dataModel.xiangmldjbj"></div>
         </div>
 
         <div class="item f14">
             <div class="label">
                 <span class="lanText" data-lanid="1000263_交付部审阅意见"></span><span>:</span>
             </div>
-            <div class="item-value">{{dataModel.jiaofbyj}}</div>
+            <div class="item-value" v-html="dataModel.jiaofbyj"></div>
         </div>
 
         <div class="item f14">
             <div class="label">
                 <span class="lanText" data-lanid="1000264_工程部审阅意见"></span><span>:</span>
             </div>
-            <div class="item-value">{{dataModel.gongcbyj}}</div>
+            <div class="item-value" v-html="dataModel.gongcbyj"></div>
         </div>
 
         <div class="item f14">
             <div class="label">
                 <span class="lanText" data-lanid="1000265_法务部审阅意见"></span><span>:</span>
             </div>
-            <div class="item-value">{{dataModel.fawbyj}}</div>
+            <div class="item-value" v-html="dataModel.fawbyj"></div>
         </div>
 
         <div class="item f14">
@@ -97,11 +97,11 @@
             </div>
             <div class="item-value">
                 <label class="checkbox-label">
-                    <input type="checkbox" v-model="dataModel.rongzbyj" name="inlineRadioOptions1" value="Bankable"/>
+                    <input type="radio" v-model="dataModel.Bankable" name="inlineRadioOptions1" value="1"/>
                     <i class="checkbox"></i><span class="radios f14">Bankable</span>
                 </label>
                 <label class="checkbox-label">
-                    <input type="checkbox" v-model="dataModel.rongzbyj" name="inlineRadioOptions1" value="NonBankable"/>
+                    <input type="radio" v-model="dataModel.NonBankable" name="inlineRadioOptions1" value="1"/>
                     <i class="checkbox"></i><span class="radios f14">Non-bankable, Limit Remains:</span>
                 </label>
             </div>
@@ -114,15 +114,15 @@
             <div class="item-value">
                 <div class="lanText paddingB5" data-lanid="1000268_批准要求"></div>
                 <label class="checkbox-label">
-                    <input type="checkbox" v-model="dataModel.dingjtdyj" name="inlineRadioOptions2" value="COO"/>
+                    <input type="radio" v-model="dataModel.COO" name="inlineRadioOptions2" value="1"/>
                     <i class="checkbox"></i><span class="radios f14">COO</span>
                 </label>
                 <label class="checkbox-label">
-                    <input type="checkbox" v-model="dataModel.dingjtdyj" name="inlineRadioOptions2" value="CEO"/>
+                    <input type="radio" v-model="dataModel.CEO" name="inlineRadioOptions2" value="1"/>
                     <i class="checkbox"></i><span class="radios f14">CEO</span>
                 </label>
                 <label class="checkbox-label">
-                    <input type="checkbox" v-model="dataModel.dingjtdyj" name="inlineRadioOptions2" value="SC"/>
+                    <input type="radio" v-model="dataModel.SC" name="inlineRadioOptions2" value="1"/>
                     <i class="checkbox"></i><span class="radios f14">SC</span>
                 </label>
             </div>
@@ -132,28 +132,28 @@
             <div class="label">
                 <span class="lanText" data-lanid="1000269_税务团队审阅意见"></span><span>:</span>
             </div>
-            <div class="item-value">{{dataModel.shuiwtdyj}}</div>
+            <div class="item-value" v-html="dataModel.shuiwtdyj"></div>
         </div>
 
         <div class="item f14">
             <div class="label">
                 <span class="lanText" data-lanid="1000270_首席运营官审阅意见"></span><span>:</span>
             </div>
-            <div class="item-value">{{dataModel.shouxyygyj}}</div>
+            <div class="item-value" v-html="dataModel.shouxyygyj"></div>
         </div>
 
         <div class="item f14">
             <div class="label">
                 <span class="lanText" data-lanid="1000271_首席商务官审批"></span><span>:</span>
             </div>
-            <div class="item-value">{{dataModel.shouxswg}}</div>
+            <div class="item-value" v-html="dataModel.shouxswg"></div>
         </div>
 
         <div class="item f14">
             <div class="label">
                 <span class="lanText" data-lanid="1000272_飞机租赁总监意见"></span><span>:</span>
             </div>
-            <div class="item-value">{{dataModel.feijzlzjyj}}</div>
+            <div class="item-value" v-html="dataModel.feijzlzjyj"></div>
         </div>
 
         <div class="item f14">
@@ -162,27 +162,27 @@
             </div>
             <div class="item-value">
                 <label class="checkbox-label">
-                    <input type="checkbox" v-model="dataModel.shouxzxgyj" name="inlineRadioOptions3" value="check1"/>
+                    <input type="radio" v-model="dataModel.check1" name="inlineRadioOptions3" value="1"/>
                     <i class="checkbox"></i><span class="radios f14">Go Ahead (Non-bankable)</span>
                 </label>
                 <label class="checkbox-label">
-                    <input type="checkbox" v-model="dataModel.shouxzxgyj" name="inlineRadioOptions3" value="rongzfs"/>
+                    <input type="radio" v-model="dataModel.rongzfs" name="inlineRadioOptions3" value="1"/>
                     <i class="checkbox"></i><span class="radios f14">Financing Method</span>
                 </label>
                 <label class="checkbox-label">
-                    <input type="checkbox" v-model="dataModel.shouxzxgyj" name="inlineRadioOptions3" value="check2"/>
+                    <input type="radio" v-model="dataModel.check2" name="inlineRadioOptions3" value="1"/>
                     <i class="checkbox"></i><span class="radios f14">Project Finance</span>
                 </label>
                 <label class="checkbox-label">
-                    <input type="checkbox" v-model="dataModel.shouxzxgyj" name="inlineRadioOptions3" value="check3"/>
+                    <input type="radio" v-model="dataModel.check3" name="inlineRadioOptions3" value="1"/>
                     <i class="checkbox"></i><span class="radios f14">Self-Funded</span>
                 </label>
                 <label class="checkbox-label">
-                    <input type="checkbox" v-model="dataModel.shouxzxgyj" name="inlineRadioOptions3" value="check5"/>
+                    <input type="radio" v-model="dataModel.check5" name="inlineRadioOptions3" value="1"/>
                     <i class="checkbox"></i><span class="radios f14">JOLCO</span>
                 </label>
                 <label class="checkbox-label">
-                    <input type="checkbox" v-model="dataModel.shouxzxgyj" name="inlineRadioOptions3" value="check4"/>
+                    <input type="radio" v-model="dataModel.check4" name="inlineRadioOptions3" value="1"/>
                     <i class="checkbox"></i><span class="radios f14">Other:</span>
                 </label>
             </div>
@@ -195,9 +195,9 @@
             <div class="item-value">
                   <div class="paddingB5">
                     <span class="lanText" data-lanid="1000275_合同签署日期"></span>
-                    <span>{{dataModel.hetqsrq}}</span>
+                    <span v-html="dataModel.hetqsrq"></span>
                   </div>
-                  <div>{{dataModel.shenhyj}}</div>
+                  <div v-html="dataModel.shenhyj"></div>
             </div>
         </div>
 
@@ -207,26 +207,23 @@
             </div>
             <div class="item-value">
                 <label class="checkbox-label">
-                    <input type="checkbox" v-model="dataModel.shenhyj1" name="inlineRadioOptions4" value="None"/>
+                    <input type="radio" v-model="dataModel.None" name="inlineRadioOptions4" value="1"/>
                     <i class="checkbox"></i><span class="radios f14">无</span>
                 </label>
                 <label class="checkbox-label">
-                    <input type="checkbox" v-model="dataModel.shenhyj1" name="inlineRadioOptions4" value="DisclosableTransaction"/>
+                    <input type="radio" v-model="dataModel.DisclosableTransaction" name="inlineRadioOptions4" value="1"/>
                     <i class="checkbox"></i><span class="radios f14">可披露交易</span>
                 </label>
                 <label class="checkbox-label">
-                    <input type="checkbox" v-model="dataModel.shenhyj1" name="inlineRadioOptions4" value="MajorTransaction"/>
+                    <input type="radio" v-model="dataModel.MajorTransaction" name="inlineRadioOptions4" value="1"/>
                     <i class="checkbox"></i><span class="radios f14">主要交易</span>
                 </label>
                 <label class="checkbox-label">
-                    <input type="checkbox" v-model="dataModel.shenhyj1" name="inlineRadioOptions4" value="VSAVSD"/>
+                    <input type="radio" v-model="dataModel.VSAVSD" name="inlineRadioOptions4" value="1"/>
                     <i class="checkbox"></i><span class="radios f14">VSA/VSD</span>
                 </label>
             </div>
         </div>
-
-
-
 
     </div>
 </div>
@@ -240,51 +237,91 @@ export default {
       title:lanTool.lanContent('1000227_LOI单号'),
       loiId:'',
       dataModel:{
-          bianh:'',
-          requestname:'',
-          loginid:'',
-          shenqrq:'',
-          jobtitlename:'',
-          departmentname:'',
-          xiangmmc:'',
-          jihqsrq:'',
-          xiangmldjbj:'',
-          jiaofbyj:'',
-          gongcbyj:'',
-          fawbyj:'',
-          rongzbyj:[],
-          dingjtdyj:[],
-          shuiwtdyj:'',
-          shouxyygyj:'',
-          shouxswg:'',
-          feijzlzjyj:'',
-          shouxzxgyj:[],
-          //合同事务经理填写:
-          hetqsrq:'',
-          shenhyj:'',
+        //   bianh:'',
+        //   requestname:'',
+        //   loginid:'',
+        //   shenqrq:'',
+        //   jobtitlename:'',
+        //   departmentname:'',
+        //   xiangmmc:'',
+        //   jihqsrq:'',
+        //   xiangmldjbj:'',
+        //   jiaofbyj:'',
+        //   gongcbyj:'',
+        //   fawbyj:'',
+        //   rongzbyj:[],
+        //   dingjtdyj:[],
+        //   shuiwtdyj:'',
+        //   shouxyygyj:'',
+        //   shouxswg:'',
+        //   feijzlzjyj:'',
+        //   shouxzxgyj:[],
+        //   //合同事务经理填写:
+        //   hetqsrq:'',
+        //   shenhyj:'',
 
-          shenhyj1:['None','VSAVSD']
+        //   shenhyj1:['None','VSAVSD']
       }
 
     }
   },
   created:function(){
       var _self = this;
-
       _self.loiId = _self.$route.params.id || '';
-      console.log(_self.loiId);
-
+      //console.log(_self.loiId);
+      //查询LOI单详情
+      _self.querySingle();
   },
   mounted:function(){
     lanTool.updateLanVersion();
   },
   methods:{
       back:function(){
-            this.$router.back(-1);
+        this.$router.back(-1);
       },
-      //
+      //查询LOI单详情
+      querySingle:function(){
+        let _self = this;
+        var urlTemp = tool.AjaxBaseUrl();
+        var controlName = tool.Api_OALOIHandle_QuerySingle;
+        //传入参数
+        var jsonDatasTemp = {
+            CurrentLanguageVersion: lanTool.currentLanguageVersion,
+            UserName: tool.UserName(),
+            _ControlName: controlName,
+            _RegisterCode: tool.RegisterCode(),
+            AutoID:_self.loiId
+        };
+        var loadingIndexClassName = tool.showLoading();
+        $.ajax({
+            async: true,
+            type: "post",
+            url: urlTemp,
+            data: jsonDatasTemp,
+            success: function (data) {
+                tool.hideLoading(loadingIndexClassName);
+                data = tool.jObject(data);
+                if (data._ReturnStatus == false) {
+                    tool.showText(tool.getMessage(data));
+                    return true;
+                }
+                data = data._OnlyOneData || {};
+                console.log(data);
+                _self.dataModel = data;
+            },
+            error: function (jqXHR, type, error) {
+                console.log(error);
+                tool.hideLoading(loadingIndexClassName);
+                return true;
+            },
+            complete: function () {
+                //隐藏虚拟键盘
+                document.activeElement.blur();
+            }
+        });
+       
+      }
   }
-
 }
 </script>
 
@@ -333,8 +370,9 @@ header.mui-bar {
   color:#000;
 }
 .item-value{
-  color:#545454;
+    color:#545454;
 }
+
 .item-value label{
   display:flex;
   padding-bottom: 5px;
@@ -349,6 +387,7 @@ label.checkbox-label .checkbox{
     height: 15px;
     border-color: #ccc;
     margin-right: 5px;
+    margin-top:2px;
 }
 label.checkbox-label input:checked+.checkbox{
     background: none;
