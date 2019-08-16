ebfa5dfd8348087d9749dbfbb624c07b9092aadf
aoniruan <aoniwen@163.com>
2019-08-16 10:43:22 +0800

    更新

--- src/assets/js/common/CommonFront.js
+++ src/assets/js/common/CommonFront.js
@@ -2,7 +2,7 @@
 		var link = document.createElement('link');
 		link.setAttribute('type', 'text/css');
 		link.setAttribute('rel', 'stylesheet');
-    link.setAttribute('href', 'https://at.alicdn.com/t/font_651046_dn0i3pevo19.css');
+    link.setAttribute('href', 'https://at.alicdn.com/t/font_651046_67w3u5uzxcd.css');
 
 		//link.setAttribute('href', '../fonts/iconfont1.css');
     document.head.appendChild(link);
