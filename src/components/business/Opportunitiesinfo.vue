48e2da777e90f1d9a1f1a948d0448ad713e37956
dylan880507 <758777282@qq.com>
2019-09-05 12:57:26 +0800

    修改opp负责人逻辑
    Signed-off-by: dylan880507 <758777282@qq.com>

--- src/components/business/Opportunitiesinfo.vue
+++ src/components/business/Opportunitiesinfo.vue
@@ -635,11 +635,20 @@ export default {
             //默认给data-field="Initiator"赋予23(公开) todo 这里要想个方法来赋值
             // 如是新增状态 默认给data-field="Initiator"赋予23(公开)
             if (_self.isAddNew) {
-                var publicObj = tool.GetPublicObj();
-                if (!tool.isNullOrEmptyObject(publicObj)) {
+                // var publicObj = tool.GetPublicObj();
+                // if (!tool.isNullOrEmptyObject(publicObj)) {
+                //     $("[data-field='IsPublic']")
+                //         .val(publicObj.text || "")
+                //         .attr("data-fieldVal", publicObj.id)
+                //         .trigger("change");
+                // }
+
+                var privateObj = tool.GetPrivateObj();
+                if (!tool.isNullOrEmptyObject(privateObj)) {
+                    $("[data-field='Initiator']").attr("data-fieldval",tool.UserAutoID()).text(tool.Realname());
                     $("[data-field='IsPublic']")
-                        .val(publicObj.text || "")
-                        .attr("data-fieldVal", publicObj.id)
+                        .val(privateObj.text || "")
+                        .attr("data-fieldVal", privateObj.id)
                         .trigger("change");
                 }
 
