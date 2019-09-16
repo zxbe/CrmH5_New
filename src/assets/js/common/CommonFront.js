45d6ee591469422bcd170baef03591feddede423
chenggengneng <253871127@qq.com>
2019-09-16 16:24:06 +0800

    提交
    Signed-off-by: chenggengneng <253871127@qq.com>

--- src/assets/js/common/CommonFront.js
+++ src/assets/js/common/CommonFront.js
@@ -2,7 +2,7 @@
 		var link = document.createElement('link');
 		link.setAttribute('type', 'text/css');
 		link.setAttribute('rel', 'stylesheet');
-    link.setAttribute('href', 'https://at.alicdn.com/t/font_651046_70o7y2rvdfe.css');
+    link.setAttribute('href', 'https://at.alicdn.com/t/font_651046_2ioe716ibon.css');
 
 		//link.setAttribute('href', '../fonts/iconfont1.css');
     document.head.appendChild(link);
