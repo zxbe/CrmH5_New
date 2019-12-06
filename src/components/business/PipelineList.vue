b9012b037de43d1701b8d59c5a6c589b1a59cd0d
chenggengneng <253871127@qq.com>
2019-12-06 09:15:31 +0800

    提交
    Signed-off-by: chenggengneng <253871127@qq.com>

--- src/components/business/PipelineList.vue
+++ src/components/business/PipelineList.vue
@@ -472,6 +472,24 @@ export default {
                   return ;
                 }
 
+                //增加字段
+                $.each(data, function(dataIndex, dataItem) {
+                  console.log(dataItem);
+                  var meetingSysmbol =
+                    lanTool.lanContent("1000001_最新的会议") || "new";
+                  var className = "";
+
+                  if (
+                    dataItem.CurrentStateNew != null &&
+                    dataItem.CurrentStateNew != undefined &&
+                    dataItem.CurrentStateNew.toString() != "38"
+                  ) {
+                    className = "closed";
+                  }
+                  _self.$set(dataItem, "className", className);
+                  _self.$set(dataItem, "meetingSysmbol", meetingSysmbol);
+                });
+
                 _self.noData = false;
                 if(queryType == 'pushLoad'){
                     _self.listData = _self.listData.concat(data);
