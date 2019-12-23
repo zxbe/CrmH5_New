1ebe8135a3713d0a175ada3c61e3aee193d8698b
chenggengneng <253871127@qq.com>
2019-12-23 17:13:36 +0800

    提交
    Signed-off-by: chenggengneng <253871127@qq.com>

--- src/components/customPlugin/Popup.vue
+++ src/components/customPlugin/Popup.vue
@@ -14,7 +14,7 @@
             <div class="row"><i class="calcfont calc-mobilephone icon"></i> <span class="lanText field-key" data-lanid="705_移动电话"></span></div>
             <div class="row"><div class="field-value can-operation" @click="callPhone">{{popupData.Phone}}</div></div>
         </div>
-        <div class="item-field">
+        <div class="item-field" style="margin-bottom:20px;">
             <div class="row"><i class="calcfont calc-mailbox icon"></i> <span class="lanText field-key" data-lanid="697_邮箱"></span></div>
             <div class="row"><div class="field-value can-operation" @click="sendEmail">{{popupData.Email}}</div></div>
         </div>
@@ -136,8 +136,17 @@ export default {
 }
 .can-operation{color:#1775cc;}
 
-.btn-div{display: flex;align-items: center;justify-content: center;margin: 0 -10px;border-top: 1px solid #cccccc;}
+.btn-div{position:relative; display: flex;align-items: center;justify-content: center;margin: 0 -10px;}
+.btn-div:after {
+    position: absolute;
+    content: "";left:0rem;right: 0rem; top: 0; height: 1px;
+    background-color: #E6E8EA;
+}
 .btn-item{padding:15px 0;flex: 1;display:flex;justify-content: center;align-items: center;}
-.send-btn{color:#f90;border-left: 1px solid #cccccc;}
-
+.send-btn{color:#f90;position: relative;}
+.send-btn:after {
+    position: absolute;
+    content: "";top:0rem;bottom: 0;left: 0; width: 1px;
+    background-color: #E6E8EA;
+}
 </style>
